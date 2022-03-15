be0a5c788e693bbd86b68bc69701292a465159fa
waltercastro171163 <walterc.100@gmail.com>
2022-03-14 20:54:50 -0500

    Talleres

---
+++ evaluacion2/script.js
@@ -0,0 +1,140 @@
+'use strict'
+
+
+// PRIMERO
+
+// let edad_1 =parseInt(prompt('digite la priemara edad'));
+// let edad_2 =parseInt(prompt('digite la segunda edad'));
+
+// if (edad_1>edad_2){
+//     console.log(`la edad mayor es ${edad_1}`)
+// }else{
+//     console.log(`la edad mayor es ${edad_2}`) }
+
+console.log('*****************************')
+
+// SEGUNDO
+
+// let nota=0;
+// let acumulador=0;
+// let promedio=0;
+// let contador=0;
+// for(let i=0; i<5;i++){
+
+//      nota=parseInt(prompt('digita nota de estudiante'));
+//      acumulador+=nota;
+//      contador++;
+//      promedio= (acumulador/contador)      
+// } 
+// console.log(`El promedio de las notas es ${promedio}`)
+
+console.log('*****************************')
+// TERCERO
+
+// let login='';
+
+// while(login != 'dom') {
+
+//      login=prompt('Digita tu login')
+
+//      if (login==='dom'){
+//           console.log('Bienvenido')
+//      } else{ 
+//           console.log('No puede ingresar, no eres un miembro')
+//      }
+
+// }
+console.log('*****************************')
+// CUATRO
+
+// let vacunado = parseInt(prompt('selecione Su estado:\n 1.Vacunado \n 2.No vacunado'));
+
+// switch (vacunado) {
+
+//      case 1: console.log('Bienvenido');
+//           break;
+
+//      case 2:
+//           console.log('Por favor ve a vacunarte');
+//           break;
+
+//      default:
+//           console.log('Por favor registrate correctamente');
+//           break;
+// }
+console.log('*****************************')
+// QUINTO
+
+// let anoActual= 2022;
+// let anoNacimiento= parseInt(prompt('Por favor igita el año de nacimiento'));
+
+// let edad =(anoActual-anoNacimiento);
+
+// console.log( `Su edad es de ${edad} `)
+console.log('*****************************')
+// SEXTO
+
+// let capital = parseInt(prompt('Capital de :\n 1. Colombia \n 2. Argentia \n 3. Peru \n 4. Japon \n 5. Alemania'));
+
+// switch (capital) {
+
+//      case 1:
+//           console.log('La capital es Bogota');
+//           break;
+//      case 2:
+//           console.log('La capital es Buenos aires');
+//           break;
+//      case 3:
+//           console.log('La capital es lima');
+//           break;
+//      case 4:
+//           console.log('La capital es Tokio');
+//           break;
+//      case 5:
+//           console.log('La capital es Berlin');
+//           break;
+//      default:
+//           console.log('Digite nuevamente la opcion deseada');
+//           break;
+// }
+console.log('*****************************')
+// SEPTIMA
+
+// let tabla=5;
+
+let tabla=parseInt(prompt('Digita el valor de la tabla que deseas ver'))
+
+console.log( `Tabla del numero ${tabla}`);
+
+for(let i=1; i<=100;i++){     
+    
+     console.log(`${tabla}*${i} = ${tabla*i}`)
+}
+
+console.log('*****************************')
+// OCTAVA
+
+
+// let traduccion = parseInt(prompt('Traducir al Ingles :\n 1. Ventana \n 2. Computador \n 3. Disco duro \n 4. Audifonos \n 5. Teclado'));
+
+// switch(traduccion){
+
+//      case 1. : 
+//      console.log( 'Window');
+//      break;
+//      case 2. : 
+//      console.log( 'Computer');
+//      break;
+//      case 3. : 
+//      console.log( 'Hard drive');
+//      break;
+//      case 4. : 
+//      console.log( 'headphones');
+//      break;
+//      case 5. : 
+//      console.log( 'screen');
+//      break;
+//      default : 
+//      console.log( 'Nuevamente digita el numero de la opcion deseada');
+//      break;
+// }
